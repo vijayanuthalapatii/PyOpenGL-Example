07e89b0d6794ae9e7ee7a6559708c0fb2affef4d
Alexandre Sampaio <allexonrails@live.com>
2013-09-20 23:52:39 -0300

    LOAD LIBRARY'S DEPENDENCIES __INIT__

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#--*-- coding:UTF-8 --*--
+
+#LOAD LIBRARY'S DEPENDENCIES
+
+import OpenGL.GL
+import OpenGL.GLU
+import OpenGL.GLUT
+
+import pygame
+import sys, os
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLU import gluLookAt
+
+from os.path import sep
+from sys import exit
+
+import math
