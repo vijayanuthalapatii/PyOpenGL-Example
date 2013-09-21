59b63424b1557df29c3950f10c1b3157d06917e0
Alexandre Sampaio <allexonrails@live.com>
2013-09-21 01:25:34 -0300

    Initialize the Examples

---
+++ OpenGl-Examples/src/__init__.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Alexandre"
+__date__ ="$21/09/2013 01:23:12$"
+
+if __name__ == "__main__":
+    print "Hello World"
