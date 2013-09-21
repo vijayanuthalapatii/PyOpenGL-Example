6c7165d6c33789d8738195bfcb2a8390f11f0e93
Alexandre Sampaio <allexonrails@live.com>
2013-09-21 14:53:03 -0300

    Removed no matched Files

--- OpenGl-Examples/src/__init__.py
+++
@@ -1,8 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-__author__="Alexandre"
-__date__ ="$21/09/2013 01:23:12$"
-
-if __name__ == "__main__":
-    print "Hello World"
---
+++ viewPorts.py
@@ -0,0 +1,4 @@
+#--*-- coding:UTF-8 --*--
+
+from __init__ import * #LOAD DEPENDENCIES FROM FILE __init__.py
+
