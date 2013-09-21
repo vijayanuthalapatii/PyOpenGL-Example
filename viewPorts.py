2b49ced5b82ab494a5c4e8db02a1205a408665ff
Alexandre Sampaio <allexonrails@live.com>
2013-09-21 14:59:03 -0300

    Begin of ViewPorts with OpenGL libs

--- viewPorts.py
+++ viewPorts.py
@@ -2,3 +2,20 @@
 
 from __init__ import * #LOAD DEPENDENCIES FROM FILE __init__.py
 
+def viewport2d():
+	pass
+
+def viewport3d():
+	pass
+
+class OpenViewPort2D(object):
+	"""OpenViewPort2D"""
+	def __init__(self, arg):
+		super(OpenViewPort2D, self).__init__()
+		self.arg = arg
+
+class OpenViewPort3D(object):
+	"""docstring for OpenViewPort3D"""
+	def __init__(self, arg):
+		super(OpenViewPort3D, self).__init__()
+		self.arg = arg
