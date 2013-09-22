3e4393dbb01eb5415e388897f3caf929d16f39ef
Alexandre Sampaio <allexonrails@live.com>
2013-09-22 15:56:01 -0300

    Spiral like an DNA With python + OpenGL - Start

---
+++ 06 - 3D-DNA.py
@@ -0,0 +1,20 @@
+# Pygame/PyopenGL
+#
+# Keywords: Alpha Blending, Textures, Animation, Double Buffer
+
+import random
+import numpy
+import Image
+import sys
+import time
+
+from __init__ import * #LOAD DEPENDENCIES FROM FILE __init__.py
+from display import Display
+from viewPorts import *
+from texture import Texture
+from math import * # trigonometria
+
+class DNA:
+  """docstring for DNA"""
+  def __init__(self, arg):
+    pass
